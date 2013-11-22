fd6d36659d4dc6c21a3bed0440539291ad0e7665
sdvic <gvw@san.rr.com>
2013-11-22 15:56:34 -0800

    Vic's version 131122A...Both wheels move backwards.

--- src/ioio/examples/hello/MainActivity.java
+++ src/ioio/examples/hello/MainActivity.java
@@ -10,17 +10,15 @@ import android.util.Log;
 import android.widget.ToggleButton;
 
 /**
- * This is the main activity of the HelloIOIO example application. It displays a
- * toggle button on the screen, which enables control of the on-board LED.
- * Modified by Vic rev 131110A
+ * This is the main activity of the HelloIOIO example application. It displays a toggle button on the screen, which
+ * enables control of the on-board LED. Modified by Vic rev 131122A
  */
 public class MainActivity extends IOIOActivity
 {
 	private ToggleButton button_;
 
 	/**
-	 * Called when the activity is first created. Here we normally initialize
-	 * our GUI.
+	 * Called when the activity is first created. Here we normally initialize our GUI.
 	 */
 	@Override
 	public void onCreate(Bundle savedInstanceState)
@@ -31,46 +29,40 @@ public class MainActivity extends IOIOActivity
 	}
 
 	/**
-	 * This is the thread on which all the IOIO activity happens. It will be run
-	 * every time the application is resumed and aborted when it is paused. The
-	 * method setup() will be called right after a connection with the IOIO has
-	 * been established (which might happen several times!). Then, loop() will
-	 * be called repetitively until the IOIO gets disconnected.
+	 * This is the thread on which all the IOIO activity happens. It will be run every time the application is resumed
+	 * and aborted when it is paused. The method setup() will be called right after a connection with the IOIO has been
+	 * established (which might happen several times!). Then, loop() will be called repetitively until the IOIO gets
+	 * disconnected
 	 */
 	class Looper extends BaseIOIOLooper
 	{
 		private DigitalOutput led_;// The on-board LED
 		private DigitalOutput motorEnable; // Both motors
 		private DigitalOutput rightMotorClock; // Step right motor
+		private DigitalOutput leftMotorClock; // Step left motor
 		private DigitalOutput motorCongtrollerReset;
 		private DigitalOutput rightMotorControl; // Motor decay mode
+		private DigitalOutput rightMotorDirection;
+		private DigitalOutput leftMotorDirection;
 
 		/**
-		 * Called every time a connection with IOIO has been established.
-		 * Typically used to open pins.
+		 * Called every time a connection with IOIO has been established. Typically used to open pins.
 		 * 
 		 * @throws ConnectionLostException
-		 *             when IOIO connection is lost.
+		 *             when IOIO connection is lost. when IOIO connection is lost.
 		 * @see ioio.lib.util.AbstractIOIOActivity.IOIOThread#setup()
 		 */
 		@Override
 		protected void setup() throws ConnectionLostException
 		{
-				led_ = ioio_.openDigitalOutput(0, false);
-				motorCongtrollerReset = ioio_.openDigitalOutput(22, true);
-				motorEnable = ioio_.openDigitalOutput(3, true);// Must be true for
-																// motors to run.
-				rightMotorClock = ioio_.openDigitalOutput(28, false);// Each pulse
-																		// moves
-																		// motor one
-																		// step.
-				rightMotorControl = ioio_.openDigitalOutput(6, false);// Both
-																		// motors.
-																		// Low =>
-																		// fast
-																		// motor
-																		// decay
-																		// mode.
+			led_ = ioio_.openDigitalOutput(0, true);
+			rightMotorDirection = ioio_.openDigitalOutput(20, true);
+			leftMotorDirection = ioio_.openDigitalOutput(21, false);
+			motorCongtrollerReset = ioio_.openDigitalOutput(22, true);
+			motorEnable = ioio_.openDigitalOutput(3, true);// Must be true for motors to run
+			rightMotorClock = ioio_.openDigitalOutput(28, false);// Each pulse moves motor one step
+			leftMotorClock = ioio_.openDigitalOutput(27, false);// Each pulse moves motor one step
+			rightMotorControl = ioio_.openDigitalOutput(6, false);// Both motors, low => fast motor decay mode
 		}
 
 		/**
@@ -85,12 +77,14 @@ public class MainActivity extends IOIOActivity
 		{
 			if (button_.isChecked())
 			{
-				led_.write(true);
+				led_.write(false);
 				try
 				{
 					Thread.sleep(1000);
 					rightMotorClock.write(true);
 					rightMotorClock.write(false);
+					leftMotorClock.write(true);
+					leftMotorClock.write(false);
 				} catch (InterruptedException e)
 				{
 				}
